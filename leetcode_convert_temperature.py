aa064afed248c69d273d4fe61a98cc94e06c73e2
OJ STUDIO LIVE <100084800+ojstudiolive@users.noreply.github.com>
2023-05-26 19:39:30 +0000

    Update leetcode_convert_temperature.py

--- leetcode_convert_temperature.py
+++ leetcode_convert_temperature.py
@@ -1,7 +1,10 @@
 class Solution:
     def convertTemperature(self, celsius: float) -> List[float]:
+        temperatures=[]
         celsius = float(celsius)
         kelvin = "{:.5f}".format(celsius + 273.15)
         far =  "{:.5f}".format(celsius *1.80 + 32.00)
         conversion_mes = f'Temperatue at {celsius} Celsius converted in Kelvin is {kelvin} and converted in Fahrenheit is {far}'
-        print(conversion_mes)
+        temperatures.append(float(kelvin))
+        temperatures.append(float(far))
+        return temperatures
